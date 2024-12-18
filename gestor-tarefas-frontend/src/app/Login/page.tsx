008e603ca034c4b83faebbfe8bc597e89e3fa9fd
Vini <126819626+vin1i@users.noreply.github.com>
2024-12-18 00:59:20 -0300

    Criação da tela de Login

---
+++ gestor-tarefas-frontend/src/app/Login/page.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import styles from "./page.module.css";
+import Image from "next/image";
+import loginImage from "../../../public/assets/LoginImage.png";
+import starImage from "../../../public/assets/star.png";  // Imagem da estrela
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";  // Importando ícones de olho
+
+const Login: React.FC = () => {
+
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(prev => !prev);
+    };
+
+    return (
+        <div className={styles.container}>
+            {/* Área da imagem */}
+            <div className={styles.imageSection}>
+                <Image src={loginImage} alt="Login Image"/>
+            </div>
+
+            {/* Área do formulário */}
+            <div className={styles.formSection}>
+                {/* Imagem da estrela acima do título */}
+                <div className={styles.starContainer}>
+                    <Image src={starImage} alt="Estrela" className={styles.starImage} />
+                </div>
+
+                <h1>Bem Vindo de Volta!</h1>
+                <p>Preencha com seus dados!</p>
+
+                <form className={styles.form} action={""}>
+                    <label className={styles.label} htmlFor="email">Email</label>
+                    <input className={styles.input} type="email" id="email" placeholder="Digite seu email"/>
+
+                    <label className={styles.label} htmlFor="password">Password</label>
+                    <div className={styles.inputWrapper}>
+                        <input
+                            className={styles.input}
+                            type={passwordVisible ? "text" : "password"}
+                            id="password"
+                            placeholder="Digite sua senha"
+                        />
+                        <span className={styles.eyeIcon} onClick={togglePasswordVisibility}>
+                            {passwordVisible ? <FaEyeSlash /> : <FaEye />}
+                        </span>
+                    </div>
+
+                    <div className={styles.options}>
+                        <div className={styles.checkbox}>
+                            <input className={styles.input} type="checkbox" id="remember"/>
+                            <label htmlFor="remember"> Lembrar por 30 dias</label>
+                        </div>
+
+                        <a href="#">Esqueceu sua senha?</a>
+                    </div>
+
+                    <button className={styles.button} type="submit">Log In</button>
+                </form>
+
+                <div className={styles.googleLogin}>
+                    <img src="https://img.icons8.com/color/48/000000/google-logo.png" alt="Google Icon" />
+                    <span> Log In com Google</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ gestor-tarefas-frontend/src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Gestão de Tarefas",
+  description: "Aplicação de Gestão de Tarefas",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
