878c11663e2c4a50e0ff2cbf24a567c2dca4578d
Vini <126819626+vin1i@users.noreply.github.com>
2024-12-22 18:27:00 -0300

    Criação da tela de Registro

---
+++ gestor-tarefas-frontend/src/app/register/page.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import styles from "./page.module.css";
+import Image from "next/image";
+import loginImage from "../../../public/assets/LoginImage.png";
+import starImage from "../../../public/assets/star.png";
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+const Register: React.FC = () => {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+
+    const togglePasswordVisibility = () => setPasswordVisible(prev => !prev);
+    const toggleConfirmPasswordVisibility = () => setConfirmPasswordVisible(prev => !prev);
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.imageSection}>
+                <Image src={loginImage} alt="Register Image"/>
+            </div>
+
+            <div className={styles.formSection}>
+                <div className={styles.starContainer}>
+                    <Image src={starImage} alt="Estrela" className={styles.starImage} />
+                </div>
+
+                <h1>Crie sua Conta!</h1>
+                <p>Preencha os campos abaixo para começar!</p>
+
+                <form className={styles.form} action={""}>
+                    <label className={styles.label} htmlFor="nome">Nome</label>
+                    <input className={styles.input} type="text" id="nome" placeholder="Digite seu nome"/>
+
+                    <label className={styles.label} htmlFor="email">Email</label>
+                    <input className={styles.input} type="email" id="email" placeholder="Digite seu email"/>
+
+                    <label className={styles.label} htmlFor="password">Senha</label>
+                    <div className={styles.inputWrapper}>
+                        <input
+                            className={styles.input}
+                            type={passwordVisible ? "text" : "password"}
+                            id="password"
+                            placeholder="Digite sua senha"
+                        />
+                        <span className={styles.eyeIcon} onClick={togglePasswordVisibility}>
+                            {passwordVisible ? <FaEyeSlash /> : <FaEye />}
+                        </span>
+                    </div>
+
+                    <label className={styles.label} htmlFor="confirmPassword">Confirme sua Senha</label>
+                    <div className={styles.inputWrapper}>
+                        <input
+                            className={styles.input}
+                            type={confirmPasswordVisible ? "text" : "password"}
+                            id="confirmPassword"
+                            placeholder="Confirme sua senha"
+                        />
+                        <span className={styles.eyeIcon} onClick={toggleConfirmPasswordVisibility}>
+                            {confirmPasswordVisible ? <FaEyeSlash /> : <FaEye />}
+                        </span>
+                    </div>
+
+                    <button className={styles.button} type="submit">Registrar</button>
+                </form>
+
+                <div className={styles.googleLogin}>
+                    <img src="https://img.icons8.com/color/48/000000/google-logo.png" alt="Google Icon" />
+                    <span>Registrar com Google</span>
+                </div>
+
+                <div className={styles.options}>
+                    <a href="#">Já possui uma conta? Faça login</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
